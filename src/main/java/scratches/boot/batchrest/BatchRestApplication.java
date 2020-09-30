7601ac9256ac2bed6e34cab3faf3fe34a068fcfd
Rashidi Zin <rashidi@zin.my>
2020-09-30 08:06:56 +0800

    Initial commit

---
+++ src/main/java/scratches/boot/batchrest/BatchRestApplication.java
@@ -0,0 +1,13 @@
+package scratches.boot.batchrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scratches/boot/batchrest/BatchRestApplicationTests.java
@@ -0,0 +1,13 @@
+package scratches.boot.batchrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
